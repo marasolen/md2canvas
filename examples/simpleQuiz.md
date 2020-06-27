c399cd041ec1c5f574e4d4e7d509cea89c263211
Harlan Colclough <harlan.colclough@gmail.com>
2020-06-27 15:22:39 -0700

    making progress

---
+++ examples/simpleQuiz.md
@@ -0,0 +1,118 @@
+---
+jupytext:
+  formats: ipynb,Rmd
+  text_representation:
+    extension: .md
+    format_name: myst
+    format_version: '0.8'
+    jupytext_version: 1.5.0
+kernelspec:
+  display_name: Python 3
+  language: python
+  name: python3
+---
+
+# Test Canvas Quiz
+Quiz description blablabla testing.
+* allowed_attempts: 3
+* scoring_policy: keep_highest
+* cant_go_back: False
+
+## First Page
+This is the first page/group.
+
+### Question
+What is the correct answer?
+* exname: Pareto
+* extype: schoice
+* exsolution: 0100
+* exshuffle: 4
+
+#### Options
+* Not this one
+* This one
+* Not this one
+* All of the above
+
+#### Solution
+The second option is the most correct.
+
+#### Options
+* False
+* True
+* False
+* False
+
+### Question
+What is the correct answer?
+* exname: Pareto
+* extype: schoice
+* exsolution: 0100
+* exshuffle: 4
+
+#### Options
+* Not this one
+* This one
+* Not this one
+* All of the above
+
+#### Solution
+The second option is the most correct.
+
+#### Options
+* False
+* True
+* False
+* False
+
+## Second Page
+This is the second page.
+The description has two lines and there are extra new lines.
+
+* attr43: attr43settings
+* anotherattr: anothersetting
+
+
+### Question
+What is the correct answer?
+* exname: Pareto
+* extype: schoice
+* exsolution: 0100
+* exshuffle: 4
+
+#### Options
+* Not this one
+* This one
+* Not this one
+* All of the above
+
+#### Solution
+The second option is the most correct.
+
+#### Options
+* False
+* True
+* False
+* False
+
+### Question
+What is the correct answer?
+* exname: Pareto
+* extype: schoice
+* exsolution: 0100
+* exshuffle: 4
+
+#### Options
+* Not this one
+* This one
+* Not this one
+* All of the above
+
+#### Solution
+The second option is the most correct.
+
+#### Options
+* False
+* True
+* False
+* False
--- myst2canvas/myst2canvas.py
+++ myst2canvas/myst2canvas.py
@@ -1,12 +1,101 @@
+import re
+import json
 import jupytext as jp
+import canvasapi as cv
+from jupytext.cli import jupytext
 
-def parse(notebook_file):
-    print(notebook_file)
-    return {}
+re_hdrtag = re.compile(r"\#{1,4}")
+hdr_dict = {"#":"quiz", "##":"group", "###":"question", "####":"subq"}
 
-def upload(quiz, token, course_id, quiz_id):
+def parse_answers(text):
+    return []
+
+def parse_attrs(text):
+    attrs = {}
+    if "\n" in text:
+        attrs["title"] = text[:text.index("\n")].strip(" \s")
+        text = text[text.index("\n") + 1:]
+    else:
+        attrs["title"] = text.strip(" \s")
+        return attrs
+
+    if "*" in text:
+        attrs["description"] = text[:text.index("*")].strip(" \s\n")
+        text = text[text.index("*"):]
+    else:
+        attrs["description"] = text.strip(" \s\n")
+        return attrs
+
+    for line in text.split("\n"):
+        if line.startswith("*"):
+            key = line[1:line.index(":")].strip(" \s")
+            val = line[line.index(":") + 1:].strip(" \s")
+            attrs[key] = val
+
+    return attrs
+
+def parse_question(text, question_arr):
+    attrs = parse_attrs(text)
+    question = {"attrs": attrs, "answers": []}
+    return question
+
+def parse_group(text, group_arr):
+    attrs = parse_attrs(text)
+    group = {"attrs": attrs, "questions": []}
+
+    for index, type, text in group_arr:
+        if hdr_dict[type] == "group":
+            break
+        elif hdr_dict[type] == "question":
+            rest = group_arr[index + 1:]
+            question = parse_question(text, rest)
+            group["questions"].append(question)
+        else:
+            continue
+
+    return group
+
+def parse_quiz(nb_file):
+    if nb_file.endswith(".ipynb"):
+        jupytext(args=[str(nb_file), "--to", "md:myst"])
+        nb_file = nb_file.replace(".ipynb", ".md")
+    nb_obj = jp.readf(nb_file)
+
+    quiz = {"attrs": {}, "groups": []}
+
+    src = nb_obj['cells'][-1]["source"]
+    hdrs = re_hdrtag.findall(src)
+    elems = re_hdrtag.split(src)[1:]
+    elems = list(map(lambda el: el.strip(), elems))
+    flat_tree = list(zip(range(len(hdrs)), hdrs, elems))
+    for index, type, text in flat_tree:
+        if hdr_dict[type] == "quiz":
+            attrs = parse_attrs(text)
+            quiz["attrs"] = attrs
+
+        elif hdr_dict[type] == "group":
+            rest = flat_tree[index + 1:]
+            group = parse_group(text, rest)
+            quiz["groups"].append(group)
+        else:
+            continue
+
+    print(print(json.dumps(quiz, indent=4)))
+
+    return quiz
+
+def get_course(url, token, course_id):
+    canvas = cv.Canvas(url, token)
+    return canvas.get_course(course_id)
+
+def edit_quiz(quiz, canvas_quiz):
     print(quiz)
-    print(token)
-    print(course_id)
-    print(quiz_id)
     return {}
+
+def upload_quiz(quiz, url, token, course_id):
+    canvas_quiz = get_course(url, token, course_id).create_quiz(quiz["attrs"])
+    return edit_quiz(quiz, canvas_quiz)
+
+def update_quiz(quiz, url, token, course_id, quiz_id):
+    canvas_quiz = get_course(url, token, course_id).get_quiz(quiz_id)
+    return edit_quiz(quiz, canvas_quiz)
